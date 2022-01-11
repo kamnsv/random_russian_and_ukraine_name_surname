6f8a9e5c34fccaeb4660857d93148cd1c79e45b7
SkippUser <84076538+SkippUser@users.noreply.github.com>
2022-01-12 00:50:48 +0300

    Убрал функцию training и переписал вызов из консоли

--- imena.py
+++ imena.py
@@ -3,88 +3,25 @@
 import sys, os, codecs
 import random
 
-
-
-
-def imfaot(ci,cf,co):
+def imfaot(pol=None, lang=None):
+	ci,cf,co = "imena_%s_%s.txt", "family_%s_%s.txt", "otch_%s_%s.txt"
+	
 	path = 'data'
 	if len(sys.argv) > 3:
 		path = sys.argv[3]
 	path += os.path.sep
+	if not pol in ['m','f']: pol = ['m','f'][random.choice(range(2))]
+	if not lang in ['ru','ua']: lang = ['ru','ua'][random.choice(range(2))]
     
 	a = []
-	for file_name in (ci,cf,co):
+	for file_name in (cf,ci,co):
+		file_name = file_name % (pol, lang)
 		with codecs.open(path+file_name, "r", "utf-8") as f:
 			lines = f.readlines()
 			line  = lines[random.choice(range(len(lines)))]
-			a.append(line.rstrip("\n"))
+			a.append(line.rstrip("\n").rstrip("\t").rstrip())
 
 	return a
 
-
-
-def training():
-	if len(sys.argv) <= 1:
-		print("Введите -m или -f")
-	elif sys.argv[1] == "-m" and sys.argv[2] == "-ru":
-		mansiru = "imena_m_ru.txt"
-		mansfru = "family_m_ru.txt"
-		mansotchru = "otch_m_ru.txt"
-
-		ci = mansiru
-		cf = mansfru
-		co = mansotchru
-		imret, famret, otchmret = imfaot(ci,cf,co)
-		print(imret)
-		print(otchmret)
-		print(famret)
-
-	elif sys.argv[1] == "-f" and sys.argv[2] == "-ru":
-		womaniru = "imena_f_ru.txt"
-		womanfru = "family_f_ru.txt"
-		womanotchru = "otch_f_ru.txt"
-
-		ci = womaniru
-		cf = womanfru
-		co = womanotchru
-
-		imret, famret, otchmret = imfaot(ci,cf,co)
-		print(imret)
-		print(otchmret)
-		print(famret)
-
-	elif sys.argv[1] == "-m" and sys.argv[2] == "-ua":
-		mansiua = "imena_m_ua.txt"
-		mansfua = "family_m_ua.txt"
-		mansotchua = "otch_m_ua.txt"
-
-		ci = mansiua
-		cf = mansfua
-		co = mansotchua
-		imret, famret, otchmret = imfaot(ci,cf,co)
-		print(imret)
-		print(otchmret)
-		print(famret)
-
-	elif sys.argv[1] == "-f" and sys.argv[2] == "-ua":
-		womaniua = "imena_f_ua.txt"
-		womanfua = "family_f_ua.txt"
-		womanotchua = "otch_f_ua.txt"
-
-		ci = womaniua
-		cf = womanfua
-		co = womanotchua
-
-		imret, famret, otchmret = imfaot(ci,cf,co)
-		print(imret)
-		print(otchmret)
-		print(famret)	
-	else:
-		print("Неправильный аргумент командной строки")
-
-	
-	return 0
-
-training()
-
-
+if "__main__" == __name__ :  # вызов из консоли
+	print(' '.join(imfaot(input('Пол [m, f]: '), input('Язык [ru, ua]: '))))
